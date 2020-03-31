cf3fa6cb8a5087454855d27af5ca9dc475d9185f
Vivian Wan <vivianpswan@gmail.com>
2020-03-31 18:32:06 +1100

    set up plan scafold

---
+++ db/migrate/20200331073009_create_planes.rb
@@ -0,0 +1,11 @@
+class CreatePlanes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :planes do |t|
+      t.text :name
+      t.integer :row
+      t.integer :column
+
+      t.timestamps
+    end
+  end
+end
