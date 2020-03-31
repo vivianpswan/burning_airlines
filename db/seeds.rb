db2dc632d3b7e91fd0bccc16a91a0eea8cadfaaf
Vivian Wan <vivianpswan@gmail.com>
2020-03-31 22:43:04 +1100

    polished the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,17 @@ p2 = Plane.create :name => 'Qantas', :row => '6', :column => '6', :user_id=> 2
 puts "#{ Plane.count } planes created"
 
 Flight.destroy_all
-f1 = Flight.create :flight_name => '747', :origin => 'Sydney', :destination => 'Singapore', :flight_date => "01-05-2020"
-f2 = Flight.create :flight_name => '757', :origin => 'Melbourne', :destination => 'Italy', :flight_date => "01-06-2020"
+f1 = Flight.create :flight_name => '747', :origin => 'Sydney', :destination => 'Singapore', :flight_date => "01-05-2020",  user_id => 1, plane_id => 1
+f2 = Flight.create :flight_name => '757', :origin => 'Melbourne', :destination => 'Italy', :flight_date => "01-06-2020", user_id => 2, plane_id => 2
+f1 = Flight.create :flight_name => '757', :origin => 'Melbourne', :destination => 'Italy', :flight_date => "01-06-2020", user_id => 3, plane_id => 1
+
 
 puts "#{ Flight.count } flights created"
 
 Reservation.destroy_all
-r1 = Reservation.create :name => 'Viv', :origin => 'Sydney', :destination => 'Singapore', :reservation_date => "01-05-2020"
-r2 = Reservation.create :name => 'Azzi', :origin => 'Melbourne', :destination => 'Italy', :reservation_date => "01-06-2020"
-r3 = Reservation.create :name => 'Kuljit', :origin => 'Sydney', :destination => 'Singapore', :reservation_date => "01-05-2020"
+r1 = Reservation.create :name => 'Viv', :origin => 'Sydney', :destination => 'Singapore', :reservation_date => "01-05-2020", user_id => 1, flight_id => 1
+r2 = Reservation.create :name => 'Azzi', :origin => 'Melbourne', :destination => 'Italy', :reservation_date => "01-06-2020", user_id => 2, flight_id => 2
+r3 = Reservation.create :name => 'Kuljit', :origin => 'Sydney', :destination => 'Singapore', :reservation_date => "01-05-2020", user_id => 3, flight_id => 1
 
 
 puts "#{ Plane.count } planes created"
@@ -39,6 +41,21 @@ u1.students << p1
 u2.students << p2
 u3.students << p1
 
-puts "Results and programs"
-r1.programs << p1
-r2.programs << p2
+puts "Users and Flights"
+u1.programs << f1
+u2.programs << f2
+u3.programs << f3
+
+puts "Users and Reservations"
+u1.programs << r1
+u2.programs << r2
+u3.programs << r3
+
+puts "Planes and Fights"
+p1.programs << f1
+p2.programs << f2
+
+puts "Flights and Reservations"
+f1.programs << r1 << r3
+f2.programs << r2
+
